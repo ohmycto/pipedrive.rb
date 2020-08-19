142c564602f55167d4196e52971df322ac83adf1
Denis Lukyanov <d.lukyanov@secoint.ru>
2020-08-19 14:30:16 +0300

    Intoducing Pipedrive::Operation#deals method.
    Returns a list of Deals for provided org_id:
    ```
    Pipedrive::Operation.new.deals(<org_id>)
    ```

--- lib/pipedrive/organization.rb
+++ lib/pipedrive/organization.rb
@@ -6,5 +6,13 @@ module Pipedrive
     include ::Pipedrive::Operations::Update
     include ::Pipedrive::Operations::Delete
     include ::Pipedrive::Utils
+
+    def deals(*args)
+      params = args.extract_options!
+      params[:org_id] ||= args[0]
+      fail 'org_id is missing' unless params[:org_id]
+      return to_enum(:deals, params) unless block_given?
+      follow_pagination(:make_api_call, [:get, "#{params[:org_id]}/deals"], params) { |item| yield item }
+    end
   end
 end
